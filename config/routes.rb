58ce9ff5787b2315ad1a212748af24dc74da7e32
Adjei Elias <adjeielias90@gmail.com>
2018-05-09 16:18:51 +0100

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   root 'pages#index'
-
-  ActiveAdmin.routes(self)
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
@@ -35,4 +33,5 @@ Rails.application.routes.draw do
 
 
 
+  ActiveAdmin.routes(self)
 end
