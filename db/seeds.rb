a4a8b1c2970952338a8f593470ec277b747a843f
Adjei Elias <adjeielias90@gmail.com>
2018-05-11 02:57:41 +0100

    Initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,4 @@ ending: Faker::Date.forward(90)
 })
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.production?
