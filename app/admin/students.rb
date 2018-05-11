9f0bf5c78294e37e78eab4fd49f2225b71c0b886
Adjei Elias <adjeielias90@gmail.com>
2018-05-11 04:55:16 +0100

    Modified admin/students.rb

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -1,15 +1,17 @@
 ActiveAdmin.register Student do
+
+    before_filter do
+        Student.class_eval do
+            def to_param
+                id.to_s
+            end
+        end
+    end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 permit_params :reference_number, :level_id, :index_number
-before_filter do
-    Student.class_eval do
-        def to_param
-            id.to_s
-        end
-    end
-end
+
 
 #
 # or
